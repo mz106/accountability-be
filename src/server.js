d551fd4bb128e9e6baa24e3bd7792dfaf2b4714f
Michael Z <michaelzwierzanski@gmail.com>
2023-01-23 18:44:56 +0000

    add server swagger code

--- src/server.js
+++ src/server.js
@@ -3,7 +3,54 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT;
 
+// swagger
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Accountability App Backend",
+      description:
+        "An app for recording and tracking goals with added accountability from friends",
+      contact: {
+        name: "Michael Z",
+      },
+      servers: ["http://localhost"],
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+
+  apis: ["src/user/routes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 const app = express();
+
+if (process.env.NODE_ENV === "dev") {
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs, {
+      explorer: true,
+    })
+  );
+}
 
 app.use(express.json());
 
