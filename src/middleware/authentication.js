6567936e9a5b20f9c448edb4be227f5fbd7df743
RaeesaParker <raeesaparker101@gmail.com>
2023-02-01 12:06:22 +0000

    Add hashing of password to authentication.js

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,1 +1,20 @@
 const bcrypt = require("bcrypt");
+
+// Salt Rounds to hash password 
+const saltRounds = 10;
+
+exports.hashPass = async (request, response, next) => {
+  try {
+    // Catch missing password
+    if (!request.body.password) {
+      response.status(500).send({ error: "A password is required",});
+      return;
+    }
+
+    // Hash the password using saltRounds defined above and save back to request body
+    request.body.password = await bcrypt.hash(request.body.password, saltRounds);
+    next();
+  } catch (error) {
+    response.status(500).send({ error: error.message });
+  }
+};
