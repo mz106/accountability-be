58a842b2dd71a4ed2049cc038aa2987a4e2af941
RaeesaParker <raeesaparker101@gmail.com>
2023-02-10 19:18:38 +0000

    #8 Change variable name of password to clear_pass to follow swagger docs

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -6,13 +6,13 @@ const saltRounds = 10;
 exports.hashPass = async (request, response, next) => {
   try {
     // Catch missing password
-    if (!request.body.password) {
+    if (!request.body.clear_pass) {
       response.status(500).send({ error: "A password is required",});
       return;
     }
 
     // Hash the password using saltRounds defined above and save back to request body
-    request.body.password = await bcrypt.hash(request.body.password, saltRounds);
+    request.body.password = await bcrypt.hash(request.body.clear_pass, saltRounds);
     next();
   } catch (error) {
     response.status(500).send({ error: error.message });
